6493ecdc5335d43084a9b61d1abded31c45b7a61
JackieCooo <1131390120@qq.com>
2022-12-21 21:39:30 +0800

    add lightbeam feature

--- Firmware/LED_Controller/include/LightbeamFeature.h
+++ Firmware/LED_Controller/include/LightbeamFeature.h
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+#define FADED_DISABLE (0)
 #define FADED_HEAD (1 << 0)
 #define FADED_TAIL (1 << 1)
 
@@ -16,13 +17,21 @@ typedef uint8_t faded_end_t;
 
 typedef enum {
     MOVE_RIGHT,
-    MODE_LEFT
+    MOVE_LEFT
 } dir_t;
 
 class LightbeamFeature {
 public:
     LightbeamFeature();
     void process(void);
+    void setColor(uint8_t r, uint8_t g, uint8_t b);
+    void setLen(uint16_t len);
+    void setInterval(uint16_t interval);
+    void setDirection(dir_t dir);
+    void setSpeed(uint16_t speed);
+    void setFadedEnd(faded_end_t faded_end);
+    void setTailLen(uint16_t len);
+    void setHeadLen(uint16_t len);
 
 private:
     RgbColor _color;
@@ -32,10 +41,13 @@ private:
     uint16_t _tail_len;
     faded_end_t _faded_end;
     dir_t _dir;
+    uint16_t _speed;  // pixels per second
     vector<RgbColor> _pattern;
-    uint16_t _base;
+    uint16_t _ref;  // the first pixel's referance to the pattern
     NeoPixelAnimator _animations = NeoPixelAnimator(1);
+    AnimUpdateCallback _cb;
 
     void _refreshPattern(void);
-    void _refreshPixels(void);
+    void _animUpdateFunc(const AnimationParam& param);
+    static uint16_t _translateSpeed(uint16_t &speed);
 };
--- Firmware/LED_Controller/src/LightbeamFeature.cpp
+++ Firmware/LED_Controller/src/LightbeamFeature.cpp
@@ -1,16 +1,78 @@
 #include <LightbeamFeature.h>
 
 LightbeamFeature::LightbeamFeature() {
+    _cb = bind(&LightbeamFeature::_animUpdateFunc, this, placeholders::_1);
+    _color = RgbColor(0);
+    _len = 0;
+    _interval = 0;
+    _dir = MOVE_RIGHT;
+    _speed = 0;
+    _faded_end = FADED_DISABLE;
+    _tail_len = 0;
+    _head_len = 0;
 }
 
 void LightbeamFeature::process(void) {
-    _animations.UpdateAnimations();
+    if (_animations.IsAnimating()) {
+        _animations.UpdateAnimations();
+    }
+    else {
+        _animations.StartAnimation(0, _speed, _cb);
+    }
+}
+
+void LightbeamFeature::setColor(uint8_t r, uint8_t g, uint8_t b) {
+    _color = RgbColor(r, g, b);
+    _refreshPattern();
+}
+
+void LightbeamFeature::setLen(uint16_t len) {
+    _len = len;
+    _refreshPattern();
+}
+
+void LightbeamFeature::setInterval(uint16_t interval) {
+    _interval = interval;
+    _refreshPattern();
+}
+
+void LightbeamFeature::setDirection(dir_t dir) {
+    _dir = dir;
+}
+
+void LightbeamFeature::setSpeed(uint16_t speed) {
+    _speed = _translateSpeed(speed);
+}
+
+void LightbeamFeature::setFadedEnd(faded_end_t faded_end) {
+    _faded_end = faded_end;
+    _refreshPattern();
+}
+
+void LightbeamFeature::setTailLen(uint16_t len) {
+    _tail_len = len;
+    _refreshPattern();
+}
+
+void LightbeamFeature::setHeadLen(uint16_t len) {
+    _head_len = len;
+    _refreshPattern();
 }
 
 void LightbeamFeature::_refreshPattern(void) {
-    uint16_t psize = _len + _interval;
+    _ref = 0;
+    uint16_t psize = _len + _interval + (_faded_end & FADED_TAIL ? _tail_len : 0) + (_faded_end & FADED_HEAD ? _head_len : 0);
     _pattern.resize(psize);
+    
     uint16_t i = 0;
+    if ((_faded_end & FADED_HEAD) && _dir == MOVE_LEFT) {
+        for (uint16_t j = 0; j < _head_len; i++) {
+            
+        }
+    }
+    else if ((_faded_end & FADED_TAIL) && _dir == MOVE_RIGHT) {
+
+    }
     for (uint16_t j = 0; j < _len; ++j) {
         _pattern[i++] = _color;
     }
@@ -19,6 +81,22 @@ void LightbeamFeature::_refreshPattern(void) {
     }
 }
 
-void LightbeamFeature::_refreshPixels(void) {
-    
+void LightbeamFeature::_animUpdateFunc(const AnimationParam& param) {
+    if (param.state == AnimationState_Started) {
+        for (uint16_t i = 0; i < strip.PixelCount(); ++i) {
+            strip.SetPixelColor(i, _pattern[(i+_ref)%_pattern.size()]);
+        }
+        strip.Show();
+        if (_dir == MOVE_LEFT) {
+            _ref = (++_ref) % _pattern.size();
+        }
+        else if (_dir == MOVE_RIGHT) {
+            _ref = _ref ? --_ref : _pattern.size() - 1;
+        }
+    }
+}
+
+uint16_t LightbeamFeature::_translateSpeed(uint16_t &speed) {
+    if (speed > 1000) return 0;
+    return (uint16_t)(1000.0 / (float)speed);
 }
